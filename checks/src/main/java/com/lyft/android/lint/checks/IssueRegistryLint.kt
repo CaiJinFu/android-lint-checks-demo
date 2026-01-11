a3f03d929469bc55ac17633bca5fca8f42130de5
猿小蔡 <cjf0898@gmail.com>
2026-01-11 18:35:45 +0800

    使用auto-service自动注册IssueRegistry

--- checks/src/main/java/com/lyft/android/lint/checks/IssueRegistryLint.kt
+++ checks/src/main/java/com/lyft/android/lint/checks/IssueRegistryLint.kt
@@ -3,6 +3,7 @@ package com.lyft.android.lint.checks
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
+import com.google.auto.service.AutoService
 
 /**
  * The class contains the list of issues that will be checked when running lint.
@@ -10,11 +11,11 @@ import com.android.tools.lint.detector.api.Issue
  * This class is referenced from the `checks/build.gradle` file, so we can
  * suppress the unused warning.
  */
+@AutoService(IssueRegistry::class)
 @Suppress("unused")
-class IssueRegistry : IssueRegistry() {
+class IssueRegistryLint : IssueRegistry() {
 
     override val api: Int = CURRENT_API
-
     override val issues: List<Issue> = listOf(
         AndroidToastJavaKotlinDetector.ISSUE,
         DeprecatedButtonJavaKotlinDetector.ISSUE,
